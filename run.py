c034f291e96724b386f3a13f2062f1e71721a7c4
Fraser Johnstone <fraser.johnstone@onthebeach.co.uk>
2020-03-31 15:23:12 +0100

    wip

--- run.py
+++ run.py
@@ -5,38 +5,34 @@ import statsmodels.api as sm
 import matplotlib.pyplot as plt
 from statsmodels.tsa.stattools import coint
 import random
-from data_extractor import get_pep_ccl
 
 def get_hedge_ratio(log_returns_a, log_returns_b):
     log_returns_a = sm.add_constant(log_returns_a)
     results = sm.OLS(log_returns_b, log_returns_a).fit()
-    log_returns_a = log_returns_a['Log returns A']
-    return results.params['Log returns A']
+    log_returns_a = log_returns_a
+    return results.params[1]
 
 def get_spreads(prices_a, prices_b, hedge_ratio):
     spreads = hedge_ratio * np.log(prices_a) - np.log(prices_b)
-    spreads.name = 'Spreads'
-
     return spreads
 
-def generate_coint_series(num_samples=100, a_shift=10, b_shift=5):
-    noise = np.random.normal(0, 1, num_samples)
+def generate_coint_series(period=100, a_shift=10, b_shift=5):
+    noise = np.random.normal(0, 1, period)
 
     prices_a = [a_shift]
 
-    for i in range(1, num_samples):
+    for i in range(1, period):
         new_price = prices_a[i-1] + np.random.normal(0, 1, 1)[0]
         while new_price <= 10:
             new_price = prices_a[i-1] + np.random.normal(0, 1, 1)[0]
         prices_a.append(new_price)
 
-    prices_a = pd.Series(prices_a, name='Prices A')
+    prices_a = pd.Series(prices_a)
     prices_b = prices_a + b_shift + noise
-    prices_b.name = 'Prices B'
 
     # un cointegrated
-    # returns_b = np.random.normal(0, 2, num_samples)
-    # prices_b = pd.Series(np.cumsum(returns_a), name='Prices A') + a_shift
+    # returns_b = np.random.normal(0, 2, period)
+    # prices_b = pd.Series(np.cumsum(returns_b), name='Prices A') + a_shift
 
     return prices_a, prices_b
 
@@ -46,53 +42,49 @@ def p_value(prices_a, prices_b):
 def z_score(spreads):
     return (spreads.iloc[-1] - spreads.mean())/spreads.std()
 
+def log_returns(prices_a, prices_b):
+    log_returns_a = (np.log(prices_a) - np.log(prices_a.shift(1)))[1:]
+    log_returns_b = (np.log(prices_b) - np.log(prices_b.shift(1)))[1:]
+    return log_returns_a, log_returns_b
+
+def get_subset(ts_a, ts_b, end_index, sample_size):
+    subset_a = ts_a[end_index-sample_size:end_index]
+    subset_b = ts_b[end_index-sample_size:end_index]
+    return subset_a, subset_b
+
+# def build_trade(price_a, price_b):
+
+
 if __name__ == '__main__':
-    num_samples = 1000000
-    lookback_period = 100
+    period = 1000
+    prices_a, prices_b = generate_coint_series(period=period)
     z_upper = 1
     z_lower = -1
     p_threshold = 1
     non_coint_threshold = 50
     trade_values = []
 
-    # prices_a, prices_b = get_pep_ccl()
-
-    prices_a, prices_b = generate_coint_series(num_samples=num_samples)
-
     currently_trading = False
-    rolling_balance = 10000.00
+    rolling_balance = 0.00
     balances = [rolling_balance]
     current_trade = {}
-    transaction_type = ''
 
-    for i in range(lookback_period, num_samples):
-        sample_prices_a = prices_a[i-lookback_period:i]
-        sample_prices_a.name = 'sample prices A'
-
-        sample_prices_b = prices_b[i-lookback_period:i]
-        sample_prices_b.name = 'sample prices B'
-
-        log_returns_a = (np.log(sample_prices_a) - np.log(sample_prices_a.shift(1)))[1:]
-        log_returns_b = (np.log(sample_prices_b) - np.log(sample_prices_b.shift(1)))[1:]
-
-        log_returns_a.name = 'Log returns A'
-        log_returns_b.name = 'Log returns B'
-
+    for i in range(period, len(prices_a)):
+        subset_prices_a, subset_prices_b = get_subset(prices_a, prices_b, i, period)
+        log_returns_a, log_returns_b = log_returns(subset_prices_a, subset_prices_b)
         hedge = get_hedge_ratio(log_returns_a, log_returns_b)
-        # print 'hedge is: ' + str(hedge)
-
-        spreads = get_spreads(sample_prices_a, sample_prices_b, hedge)
+        spreads = get_spreads(subset_prices_a, subset_prices_b, hedge)
         zscore = z_score(spreads)
-        p_val = p_value(sample_prices_a, sample_prices_b)
+        p_val = p_value(subset_prices_a, subset_prices_b)
 
         if not currently_trading:
             if p_val <= p_threshold:
                 if zscore > z_upper:
                     # the spread is more than the mean so we short A and long B
-                    wallet_delta = hedge * sample_prices_a.iloc[-1] - sample_prices_b.iloc[-1]
+                    wallet_delta = hedge * subset_prices_a.iloc[-1] - subset_prices_b.iloc[-1]
                     current_trade = {
-                        'open_price_a': sample_prices_a.iloc[-1],
-                        'open_price_b': sample_prices_b.iloc[-1],
+                        'open_price_a': subset_prices_a.iloc[-1],
+                        'open_price_b': subset_prices_b.iloc[-1],
                         'hedge_ratio': hedge,
                         'longing': False,
                         'shorting': True,
@@ -104,10 +96,10 @@ if __name__ == '__main__':
                     currently_trading = True
                 elif zscore < z_lower:
                     # the spread is less than the mean so we short B and long A
-                    wallet_delta = sample_prices_b.iloc[-1] - hedge * sample_prices_a.iloc[-1]
+                    wallet_delta = subset_prices_b.iloc[-1] - hedge * subset_prices_a.iloc[-1]
                     current_trade = {
-                        'open_price_a': sample_prices_a.iloc[-1],
-                        'open_price_b': sample_prices_b.iloc[-1],
+                        'open_price_a': subset_prices_a.iloc[-1],
+                        'open_price_b': subset_prices_b.iloc[-1],
                         'hedge_ratio': hedge,
                         'longing': True,
                         'shorting': False,
@@ -120,18 +112,14 @@ if __name__ == '__main__':
         else:
             if p_val > p_threshold:
                 if current_trade['non_coint_count'] > non_coint_threshold:
-                    print
-                    print 'closing for long running non cointegration'
                     if current_trade['shorting']:
-                        wallet_delta = sample_prices_b.iloc[-1] - current_trade['hedge_ratio'] * sample_prices_a.iloc[-1]
+                        wallet_delta = subset_prices_b.iloc[-1] - current_trade['hedge_ratio'] * subset_prices_a.iloc[-1]
                         rolling_balance += wallet_delta
-                        print str(i) + '. rolling balance: ' + str(rolling_balance)
                         current_trade = {}
                         currently_trading = False
                     elif current_trade['longing']:
-                        wallet_delta = current_trade['hedge_ratio'] * sample_prices_a.iloc[-1] - sample_prices_b.iloc[-1]
+                        wallet_delta = current_trade['hedge_ratio'] * subset_prices_a.iloc[-1] - subset_prices_b.iloc[-1]
                         rolling_balance += wallet_delta
-                        print str(i) + '. rolling balance: ' + str(rolling_balance)
                         current_trade = {}
                         currently_trading = False
                     continue
@@ -139,15 +127,13 @@ if __name__ == '__main__':
                     current_trade['non_coint_count'] += 1
 
             if current_trade['shorting'] and zscore < z_lower:
-                wallet_delta = sample_prices_b.iloc[-1] - sample_prices_a.iloc[-1] * current_trade['hedge_ratio']
+                wallet_delta = subset_prices_b.iloc[-1] - subset_prices_a.iloc[-1] * current_trade['hedge_ratio']
                 rolling_balance += wallet_delta
-                print str(i) + '. rolling balance: ' + str(rolling_balance)
                 current_trade = {}
                 currently_trading = False
             elif current_trade['longing'] and zscore > z_upper:
-                wallet_delta = current_trade['hedge_ratio'] * sample_prices_a.iloc[-1] - sample_prices_b.iloc[-1]
+                wallet_delta = current_trade['hedge_ratio'] * subset_prices_a.iloc[-1] - subset_prices_b.iloc[-1]
                 rolling_balance += wallet_delta
-                print str(i) + '. rolling balance: ' + str(rolling_balance)
                 current_trade = {}
                 currently_trading = False
 
